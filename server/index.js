38fc57daaa40393c805b0f6a71d99e2fd50f1e53
Eyüp Can Esen <eyupcanee@gmail.com>
2024-01-21 19:34:05 +0300

    Installed necessary modules. Server started.

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+
+dotenv.config({ path: "./.env.development.local" });
+
+// APP Configration
+const app = express();
+
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// Mongoose Configration
+const PORT = process.env.PORT || 8000;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port : ${PORT}`));
+  })
+  .catch((error) => {
+    console.log(`Error : ${error}. Server has not connected!`);
+  });
