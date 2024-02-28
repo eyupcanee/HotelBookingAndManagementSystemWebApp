e7fa6a18b3362c118207ef4222f487e3eb06a24e
Eyüp Can Esen <eyupcanee@gmail.com>
2024-02-28 21:01:12 +0300

    Authorize.js coded for admin authorize just for know. New roles will be added.

---
+++ server/util/Authorize.js
@@ -0,0 +1,19 @@
+import jwtDecode from "jwt-decode";
+
+export const authorizeAdmin = async (token) => {
+  try {
+    if (jwtDecode(token).role == "admin") return true;
+    else return false;
+  } catch (error) {
+    return false;
+  }
+};
+
+export const getAdminId = async (token) => {
+  try {
+    if (jwtDecode(token).role == "admin") return jwtDecode(token).id;
+    else return null;
+  } catch (error) {
+    return null;
+  }
+};
