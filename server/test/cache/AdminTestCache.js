529ba10b3297d58f5fb779cf493665a2604a770d
Eyüp Can Esen <eyupcanee@gmail.com>
2024-03-02 20:13:10 +0300

    AdminTestCache fixed.

--- server/test/cache/AdminTestCache.js
+++ server/test/cache/AdminTestCache.js
@@ -1,5 +1,6 @@
 import redisClient from "../../cache/RedisConfigration";
 import AdminTest from "../models/AdminTest";
+import { authorizeAdmin, getAdminId } from "../../util/Authorize.js";
 
 (async () => {
   try {
@@ -8,40 +9,52 @@ import AdminTest from "../models/AdminTest";
 })();
 
 export const getTestAdminCached = async (req, res, next) => {
-  const { id } = req.params;
-  let results;
-  try {
-    const cacheResults = await redisClient.get(id);
-    if (cacheResults) {
-      results = JSON.parse(cacheResults);
-      res.status(200).json({ status: "ok", fromCache: true, data: results });
-    } else {
+  const { token } = req.params;
+  if (await authorizeAdmin(token)) {
+    const { id } = req.params;
+    let results;
+    try {
+      const cacheResults = await redisClient.get(id);
+      if (cacheResults) {
+        results = JSON.parse(cacheResults);
+        res.status(200).json({ status: "ok", fromCache: true, data: results });
+      } else {
+        next();
+      }
+    } catch (error) {
       next();
     }
-  } catch (error) {
-    next();
+  } else {
+    res.status(404).json({ status: "no", message: "Unauthorized process!" });
   }
 };
 
 export const getAllAdminsCached = async (req, res, next) => {
-  const hashKey = "testadmins";
-  try {
-    const allAdminsFromDB = await AdminTest.countDocuments();
-    const cacheAdminCount = await redisClient.hlen(hashKey);
+  const { token } = req.params;
+  if (await authorizeAdmin(token)) {
+    const hashKey = "testadmins";
+    try {
+      const allAdminsFromDB = await AdminTest.countDocuments();
+      const cacheAdminCount = await redisClient.hlen(hashKey);
 
-    if (cacheAdminCount == allAdminsFromDB) {
-      redisClient.hgetall(hashKey, (error, results) => {
-        if (error) {
-          res.status(404).json({ status: "no", message: error.message });
-        } else {
-          const data = Object.values(results).map((value) => JSON.parse(value));
-          res.status(200).json({ status: "ok", fromCache: true, data: data });
-        }
-      });
-    } else {
+      if (cacheAdminCount == allAdminsFromDB) {
+        redisClient.hgetall(hashKey, (error, results) => {
+          if (error) {
+            res.status(404).json({ status: "no", message: error.message });
+          } else {
+            const data = Object.values(results).map((value) =>
+              JSON.parse(value)
+            );
+            res.status(200).json({ status: "ok", fromCache: true, data: data });
+          }
+        });
+      } else {
+        next();
+      }
+    } catch (error) {
       next();
     }
-  } catch (error) {
-    next();
+  } else {
+    res.status(404).json({ status: "no", message: "Unauthorized process!" });
   }
 };
