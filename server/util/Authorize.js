12fd5dfc1f23bbb2f0d3c69db3e141b23ba22c52
Eyüp Can Esen <eyupcanee@gmail.com>
2024-03-02 23:14:48 +0300

    New authorization functions added to Authorize.js.

--- server/util/Authorize.js
+++ server/util/Authorize.js
@@ -17,3 +17,39 @@ export const getAdminId = async (token) => {
     return null;
   }
 };
+
+export const authorizeUser = async (token) => {
+  try {
+    if (jwtDecode(token).role == "user") return true;
+    else return false;
+  } catch (error) {
+    return false;
+  }
+};
+
+export const getUserId = async (token) => {
+  try {
+    if (jwtDecode(token).role == "user") return jwtDecode(token).id;
+    else return null;
+  } catch (error) {
+    return null;
+  }
+};
+
+export const authorizeHotelManager = async (token) => {
+  try {
+    if (jwtDecode(token).role == "hmanager") return true;
+    else return false;
+  } catch (error) {
+    return false;
+  }
+};
+
+export const getHotelManagerId = async (token) => {
+  try {
+    if (jwtDecode(token).role == "hmanager") return jwtDecode(token).id;
+    else return null;
+  } catch (error) {
+    return null;
+  }
+};
